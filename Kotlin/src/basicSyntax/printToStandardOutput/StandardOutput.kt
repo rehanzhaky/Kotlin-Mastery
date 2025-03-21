fda7ca01ea62cb15756b9a223723f7963e6ec6d7
rehanzhaky <hannn9491@gmail.com>
2025-03-21 10:56:53 +0700

    mod: Print to Standard Output

---
+++ Kotlin/src/basicSyntax/printToStandardOutput/StandardOutput.kt
@@ -0,0 +1,11 @@
+package basicSyntax.printToStandardOutput
+
+fun main() {
+//    Penerapan print tanpa membuat baris baru
+    print("Hello, ")
+    print("World!")
+
+//    Penerapan print dengan membuat baris baru
+    println("Hello, World!")
+    println("Hello, New Line!")
+}
