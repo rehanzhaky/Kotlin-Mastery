daeaddca9e72fce9088f6563aeff983c09d03d5f
rehanzhaky <hannn9491@gmail.com>
2025-03-21 10:58:43 +0700

    mod: Read From Standard Input - Theory

---
+++ Kotlin/src/basicSyntax/readFromStandardInput/StandardInput.kt
@@ -0,0 +1,13 @@
+package basicSyntax.readFromStandardInput
+
+fun main() {
+//    Untuk meletak intruksi
+    println("Input ur message = ")
+
+//    Untuk menyimpan hasil inputan
+    val storeText = readln()
+
+//    Untuk menampilkan hasil inputan
+    println("ur message is: ")
+    println(storeText)
+}
