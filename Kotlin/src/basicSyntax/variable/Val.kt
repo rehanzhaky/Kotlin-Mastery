541f60054d3a81ddeaebec0bb82bf3783cb8066a
rehanzhaky <hannn9491@gmail.com>
2025-03-22 15:09:43 +0700

    mod: Variable Val

---
+++ Kotlin/src/basicSyntax/variable/Val.kt
@@ -0,0 +1,12 @@
+package basicSyntax.variable
+
+fun main() {
+//    keyword val tidak bisa diganti value nya, hanya bisa sekali diinisialisasi
+    val x = 5
+    println(x)
+
+//    ketika dipaksa untuk merubah value yang sudah diinisialisasikan, maka akan menampilkan error
+//    hapus komentar di baris 10 untuk melihat error dari kode tersebut
+//    x = 6
+
+}
