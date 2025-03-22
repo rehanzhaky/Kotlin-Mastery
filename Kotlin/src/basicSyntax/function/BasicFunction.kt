022a090f5323a7f4512376b4f67056e48cf3c295
rehanzhaky <hannn9491@gmail.com>
2025-03-22 14:48:05 +0700

    mod: update Basic Function

--- Kotlin/src/basicSyntax/function/BasicFunction.kt
+++ Kotlin/src/basicSyntax/function/BasicFunction.kt
@@ -1,9 +1,11 @@
 package basicSyntax.function
 
+// tempat untuk memanggil function yang sudah kita buat
 fun main() {
     println("Total 3 - 5 adalah : ${pengurangan(3,5)}")
 }
 
+// function yang mempunyai body block
 fun pengurangan(a: Int, b: Int): Int {
     return a - b
 }
